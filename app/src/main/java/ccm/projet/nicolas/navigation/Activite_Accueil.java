f414022f34ef2b66e7294b97e6384dc0554e711a
Nicolas0verd3nineji <verdiere.njl@gmail.com>
2018-10-19 13:07:00 +0200

    Base Projet

---
+++ app/src/main/java/ccm/projet/nicolas/navigation/Activite_Accueil.java
@@ -0,0 +1,13 @@
+package ccm.projet.nicolas.navigation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activite_Accueil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_activite_accueil);
+    }
+}
